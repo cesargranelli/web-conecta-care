b47446c8479c5e698a95f9ce01506027d91ee086
Cesar Granelli <cesar.granelli.dev@gmail.com>
2020-06-06 02:08:56 -0300

    feature/implementacao-formulario-reativo-cadastro - criacao do formulario reativo para paciente - implementacao da validacao do cpf - implementacao das validacoes de cpf e cnpj de paciente, profissional, homecare e convenio - criacao da classe utils para cpf e cnpj - implementacao das validacoes para cpf e cnpj - correcao do email de contato
    ajuste na cor do botao cadastrar enquanto o formulario estiver invalido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MenuComponent } from './menu/menu.component';
 import { ConnectaComponent } from './connecta/connecta.component';
 import { SigninComponent } from './signin/signin.component';
+import { ValidadorCpf } from './utils/validador-cpf.utils';
+import { ValidadorCnpj } from './utils/validador-cnpj.utils';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,14 @@ import { SigninComponent } from './signin/signin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgxMaskModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    ValidadorCpf,
+    ValidadorCnpj
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/utils/validador-cpf.utils.ts
@@ -0,0 +1,40 @@
+export class ValidadorCpf {
+
+  private primeiroDigito: number;
+  private segundoDigito: number;
+  private modulo11: number = 11;
+
+  validar(numero: string): boolean {
+    let pesoPrimeiroDV: number[] = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+    let pesoSegundoDV: number[] = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+    this.primeiroDigito = this.digitoVerificador(numero, pesoPrimeiroDV);
+    this.segundoDigito = this.digitoVerificador(numero.concat(String(this.primeiroDigito)), pesoSegundoDV);
+    let digitoVerificador: string = String(this.primeiroDigito).concat(
+      String(this.segundoDigito)
+    );
+    if (digitoVerificador == numero.substr(9, 2)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private digitoVerificador(digitos: string, peso: number[]): number {
+    let digito: number;
+    let somaDigito: number = Number.parseInt(
+      digitos.split('', peso.length).reduce((soma, valor, index) => {
+        return String(
+          Number.parseInt(soma) * (index == 1 ? peso[index - 1] : 1) + Number.parseInt(valor) * peso[index]
+        );
+      })
+    );
+    let resto: number = somaDigito % this.modulo11;
+    if (resto < 2) {
+      digito = 0;
+    } else {
+      digito = this.modulo11 - resto;
+    }
+    return digito;
+  }
+
+}
