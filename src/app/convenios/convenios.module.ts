403f0956e66322ab4e642f79898711f22c375d5b
cesar.granelli.dev@gmail.com <cesar.granelli.dev@gmail.com>
2020-07-09 14:20:01 -0300

    criacao do modulo e tela inicial para convenios

--- src/app/convenios/convenios.module.ts
+++ src/app/convenios/convenios.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ConveniosComponent } from './convenios.component';
 
-
+const routes: Routes = [
+  { path: 'convenios/:id', component: ConveniosComponent }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [ConveniosComponent],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ]
 })
