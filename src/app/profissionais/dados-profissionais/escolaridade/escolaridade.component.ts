2674339dad4658bff80b8f4cc0ba51de48078719
unknown <eusouoganuto@gmail.com>
2020-08-06 22:54:08 -0300

    melhorias nos componentes da info bancaria e finalizacao info academica

--- src/app/profissionais/dados-profissionais/escolaridade/escolaridade.component.ts
+++ src/app/profissionais/dados-profissionais/escolaridade/escolaridade.component.ts
@@ -1,107 +1,134 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Navigation, Router} from '@angular/router';
+import {Router} from '@angular/router';
 import {Escolaridade} from 'src/app/classes/escolaridade.class';
 import {Instrucao} from 'src/app/classes/instrucao.class';
 import {DominioService} from 'src/app/services/dominio.service';
 import {EscolaridadeService} from 'src/app/services/escolaridade.service';
 import {Valid} from 'src/app/services/feat/Valid';
-import Swal from 'sweetalert2';
 import {InputValidationHas} from 'src/app/shared/validations/input-validation-has';
 import {SharedLoadingService} from 'src/app/shared/services/shared-loading.service';
 import {CadastroProfissionaisService} from 'src/app/services/cadastro-profissionais.service';
+import {ValidService} from '../../../shared/services/shared-valid.service';
+import {Subscription} from 'rxjs';
+import Swal from 'sweetalert2';
+import {concatMap, map} from 'rxjs/operators';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-escolaridade',
   templateUrl: './escolaridade.component.html',
   styleUrls: ['./escolaridade.component.css']
 })
-export class EscolaridadeComponent implements OnInit {
-
+export class EscolaridadeComponent implements OnInit, OnDestroy {
   @Output() loadingEvent = new EventEmitter<boolean>();
 
-  escolaridadeForm: FormGroup;
+  private _dadosLocalStorage: Valid;
+  private _serviceSubscription: Subscription;
+  private _dominioSubscription: Subscription;
+  private _flatSubscription: Subscription;
 
-  private _valid: Valid;
-  private _escolaridade: Escolaridade = new Escolaridade();
-
-  public instrucoes: Instrucao[] = [];
-  public validationHas: InputValidationHas = new InputValidationHas();
+  public instrucoes: Array<Instrucao>;
+  public validationHas: InputValidationHas;
+  public escolaridadeForm: FormGroup;
+  public escolaridade: Escolaridade;
 
   constructor(
     private _router: Router,
     private _formBuilder: FormBuilder,
     private _dominioService: DominioService,
+    private _validService: ValidService,
     private _service: EscolaridadeService,
     private _sharedLoadingService: SharedLoadingService,
     private _cadastro: CadastroProfissionaisService
   ) {
-    const navigation: Navigation = this._router.getCurrentNavigation();
-    this._valid = navigation.extras.state?.valid;
     this.escolaridadeForm = this._formBuilder.group({
-      instrucao: ['', [Validators.required]],
-      instituicaoEnsino1: ['', [Validators.maxLength(50)]],
-      anoConclusao1: ['', [Validators.maxLength(4)]],
-      instituicaoEnsino2: ['', [Validators.maxLength(50)]],
-      anoConclusao2: ['', [Validators.maxLength(4)]],
-      instituicaoEnsino3: ['', [Validators.maxLength(50)]],
-      anoConclusao3: ['', [Validators.maxLength(4)]],
+      instrucao: [null, Validators.required],
+      instituicaoEnsino1: [null, Validators.maxLength(50)],
+      anoConclusao1: [null, Validators.maxLength(4)],
+      instituicaoEnsino2: [null, Validators.maxLength(50)],
+      anoConclusao2: [null, Validators.maxLength(4)],
+      instituicaoEnsino3: [null, Validators.maxLength(50)],
+      anoConclusao3: [null, Validators.maxLength(4)],
     });
   }
 
   ngOnInit(): void {
-    // if (this?._valid?.role != Role.Profissional || !this?._valid?.role) {
-    //   this._router.navigateByUrl('/');
-    // }
+    this.validationHas = new InputValidationHas();
+    this.escolaridade = new Escolaridade();
+    this._dadosLocalStorage = this._validService.getValid();
 
-    // this._dominioService.getInstrucoes().subscribe(response => {
-    //   this.instrucoes = response.body
-    // });
+    this._dominioService.getInstrucoes().pipe(
+      map(instrucoes => {
+        this.instrucoes = instrucoes.body;
+      }),
+      concatMap(() => this._service.getDados(this._dadosLocalStorage.id))
+    ).subscribe(escolaridade => {
+      this.escolaridade = escolaridade;
+      this.popularForm();
+      setTimeout(() => {
+        jQuery('select[id=\'instrucao\']').selectpicker('refresh');
+      });
+    });
 
+  }
 
+  popularForm() {
+    this.escolaridadeForm.patchValue({
+      instrucao: this.escolaridade.instrucao,
+      instituicaoEnsino1: this.escolaridade.instituicaoEnsino[0],
+      anoConclusao1: this.escolaridade.anoConclusao[0],
+      instituicaoEnsino2: this.escolaridade.instituicaoEnsino[1],
+      anoConclusao2: this.escolaridade.anoConclusao[1],
+      instituicaoEnsino3: this.escolaridade.instituicaoEnsino[2],
+      anoConclusao3: this.escolaridade.anoConclusao[2]
+    });
   }
 
   onSubmit() {
-    console.log(this.escolaridadeForm.value);
+    this._sharedLoadingService.emitChange(true);
 
-    // this._sharedLoadingService.emitChange(true);
-    // this._escolaridade.instrucao = this.escolaridadeForm.value.instrucao;
-    //
-    // this._escolaridade.instituicaoEnsino.push(this.escolaridadeForm.value.instituicaoEnsino1);
-    // this._escolaridade.instituicaoEnsino.push(this.escolaridadeForm.value.instituicaoEnsino2);
-    // this._escolaridade.instituicaoEnsino.push(this.escolaridadeForm.value.instituicaoEnsino3);
-    //
-    // this._escolaridade.anoConclusao.push(this.escolaridadeForm.value.anoConclusao1);
-    // this._escolaridade.anoConclusao.push(this.escolaridadeForm.value.anoConclusao2);
-    // this._escolaridade.anoConclusao.push(this.escolaridadeForm.value.anoConclusao3);
-    //
-    // this._escolaridade.proprietarioId = this._valid.id;
-    //
-    // this._service.save(this._escolaridade).subscribe(response => {
-    //     setTimeout(() => {
-    //       this._cadastro.escolaridade = this._escolaridade;
-    //       this._router.navigateByUrl(`cadastro/profissionais/${this._valid.id}/complemento`, {
-    //         state: {valid: this._valid}
-    //       });
-    //       this._sharedLoadingService.emitChange(false);
-    //     });
-    //   },
-    //   (error: Error) => {
-    //     this._sharedLoadingService.emitChange(false);
-    //     Swal.fire({
-    //       position: 'center',
-    //       icon: 'error',
-    //       title: 'Ocorreu um erro inexperado ao tentar inserir escolaridade',
-    //       showConfirmButton: true
-    //     });
-    //   });
+    this.escolaridade.instrucao = this.escolaridadeForm.value.instrucao;
+    this.escolaridade.instituicaoEnsino[0] = this.escolaridadeForm.value.instituicaoEnsino1;
+    this.escolaridade.instituicaoEnsino[1] = this.escolaridadeForm.value.instituicaoEnsino2;
+    this.escolaridade.instituicaoEnsino[2] = this.escolaridadeForm.value.instituicaoEnsino3;
+    this.escolaridade.anoConclusao[0] = this.escolaridadeForm.value.anoConclusao1;
+    this.escolaridade.anoConclusao[1] = this.escolaridadeForm.value.anoConclusao2;
+    this.escolaridade.anoConclusao[2] = this.escolaridadeForm.value.anoConclusao3;
+    this.escolaridade.proprietarioId = this._dadosLocalStorage.id;
 
+    this._service.save(this.escolaridade).subscribe({
+      next: () => {
+        setTimeout(() => {
+          this._cadastro.escolaridade = this.escolaridade;
+          this._router.navigateByUrl(`cadastro/profissionais/${this._dadosLocalStorage.id}/complemento`, {
+            state: {valid: this._dadosLocalStorage}
+          });
+          this._sharedLoadingService.emitChange(false);
+        });
+      },
+      error: () => {
+        this._sharedLoadingService.emitChange(false);
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Ocorreu um erro inexperado ao tentar inserir escolaridade',
+          showConfirmButton: true
+        });
+      }
+    });
   }
 
   onReturn() {
-    this._router.navigateByUrl(`cadastro/profissionais/${this._valid.id}/experiencia`, {
-      state: {valid: this._valid}
+    this._router.navigateByUrl(`cadastro/profissionais/${this._dadosLocalStorage.id}/experiencia`, {
+      state: {valid: this._dadosLocalStorage}
     });
+  }
+
+  ngOnDestroy() {
+    this._serviceSubscription.unsubscribe();
+    this._dominioSubscription.unsubscribe();
   }
 
 }
