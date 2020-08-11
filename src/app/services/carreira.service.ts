872db4e073344aa391c6fe1b23f78e517f06c2f1
unknown <eusouoganuto@gmail.com>
2020-08-11 01:24:00 -0300

    Consulta finalizada e testada; inclusao finalizada, apenas falta validar mais

--- src/app/services/carreira.service.ts
+++ src/app/services/carreira.service.ts
@@ -1,9 +1,11 @@
-import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from '../../../src/environments/environment';
-import { Carreira } from '../classes/carreira.class';
-import { TokenService } from './token.service';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
+import {Carreira} from '../classes/carreira.class';
+import {TokenService} from './token.service';
+import {map} from 'rxjs/operators';
+import {CarreiraResponseInterface} from './response/carreiraResponse.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -20,9 +22,19 @@ export class CarreiraService {
     this._headers = this._headers.set('Token', 'Bearer ' + this._tokenService.getToken());
   }
 
+  getDados(id: number): Observable<Carreira> {
+    return this._http.get(`${this.endpoint}/${id}`,
+      {
+        headers: this._headers
+      }).pipe(map((carreira: CarreiraResponseInterface) => {
+        return carreira.data;
+      })
+    );
+  }
+
   save(payload: Carreira): Observable<HttpResponse<any>> {
     return this._http.post<HttpResponse<any>>(`${this.endpoint}`, payload,
-      { headers: this._headers, observe: 'response' });
+      {headers: this._headers, observe: 'response'});
   }
 
 }
