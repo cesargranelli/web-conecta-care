04b814c74788ef3886de32d4c905466e84c794be
cesar.granelli.dev@gmail.com <cesar.granelli.dev@gmail.com>
2020-08-12 22:29:21 -0300

    corrigido campo rg para permitir valores alfanumericos

--- src/app/components/cadastro/profissional/informacoes-gerais/informacoes-gerais.component.ts
+++ src/app/components/cadastro/profissional/informacoes-gerais/informacoes-gerais.component.ts
@@ -1,11 +1,14 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import {
-  AbstractControl,
   FormBuilder,
-  FormGroup,
-  Validators,
+
+
+  FormControl, FormGroup,
+  Validators
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { concatMap } from 'rxjs/internal/operators/concatMap';
+import { map } from 'rxjs/internal/operators/map';
 import { EstadoCivil } from 'src/app/classes/estado-civil.class';
 import { Genero } from 'src/app/classes/genero.class';
 import { Profissional } from 'src/app/classes/profissional.class';
@@ -67,82 +70,84 @@ export class InformacoesGeraisComponent implements OnInit {
     private _cadastro: CadastroProfissionaisService
   ) {
     this.valid = this._validService.getValid();
+
+    this.profissionalForm = this._formBuilder.group({
+      nome: [null, [Validators.required]],
+      sobrenome: [null, [Validators.required, Validators.maxLength(60)]],
+      dataNascimento: [null, [Validators.required]],
+      rg: [null],
+      rgEmissor: [null],
+      rgDataEmissao: [null],
+      pis: [null],
+      genero: [null, [Validators.required]],
+      tipoEmpresa: [null, [Validators.required]],
+      estadoCivil: [null, [Validators.required]],
+      cnpj: [null, [Validators.required, validCnpj()]],
+      ctps: [null, [Validators.required]],
+      ctpsSerie: [null, [Validators.required]],
+      fotoProfissional: [null, [Validators.required]],
+      fotoRg: [null, [Validators.required]],
+    });
   }
 
   ngOnInit(): void {
-    this._loading.emitChange(true);
-
     if (this?.valid?.role != Role.Profissional || !this?.valid?.role) {
       // this._router.navigateByUrl('/');
     }
 
-    this._dominioService.getGeneros().subscribe(
-      (response) => {
+    this._dominioService.getGeneros().pipe(
+      map((response) => {
+        this._loading.emitChange(true);
         this.generos = response.body;
-      },
-      null,
-      () => {
-        setTimeout(() => {
-          jQuery("select[id='genero']").selectpicker('refresh');
-        });
-      }
-    );
+      }),
+      concatMap(() => this._dominioService.getTipoEmpresas().pipe(
+        map(response => {
+          this.tipoEmpresas = response.body;
+        }))
+      ),
+      concatMap(() => this._dominioService.getEstadoCivis().pipe(
+        map(response => {
+          this.estadoCivis = response.body;
+        }))
+      )
+    ).subscribe(() => {
+      this.popularForm();
+      setTimeout(() => {
+        jQuery(`select[id='genero']`).selectpicker('refresh');
+        jQuery(`select[id='tipoEmpresa']`).selectpicker('refresh');
+        jQuery("select[id='estadoCivil']").selectpicker('refresh');
+        this._loading.emitChange(false);
+      });
+    }, () => {
+      this._loading.emitChange(false);
+    });
 
-    this._dominioService.getTipoEmpresas().subscribe(
-      (response) => {
-        this.tipoEmpresas = response.body;
-      },
-      null,
-      () => {
-        setTimeout(() => {
-          jQuery("select[id='tipoEmpresa']").selectpicker('refresh');
-        });
-      }
-    );
-
-    this._dominioService.getEstadoCivis().subscribe(
-      (response) => {
-        this.estadoCivis = response.body;
-      },
-      null,
-      () => {
-        setTimeout(() => {
-          jQuery("select[id='estadoCivil']").selectpicker('refresh');
-        });
-      }
-    );
-
-    this.profissionalForm = this._formBuilder.group({
-      nome: [this._cadastro.profissional?.nome, [Validators.required]],
-      sobrenome: [
-        this._cadastro.profissional?.sobrenome,
-        [Validators.required, Validators.maxLength(60)],
-      ],
-      dataNascimento: [
-        this._cadastro.profissional?.dataNascimento,
-        [Validators.required],
-      ],
-      rg: [this._cadastro.profissional?.rg],
-      rgEmissor: [this._cadastro.profissional?.rgEmissor],
-      rgDataEmissao: [this._cadastro.profissional?.rgDataEmissao],
-      pis: [this._cadastro.profissional?.pis],
-      genero: ['', [Validators.required]],
-      tipoEmpresa: ['', [Validators.required]],
-      estadoCivil: ['', [Validators.required]],
-      cnpj: [
-        this._cadastro.profissional?.cnpj,
-        [Validators.required, validCnpj()],
-      ],
-      ctps: [this._cadastro.profissional?.ctps, [Validators.required]],
-      ctpsSerie: [
-        this._cadastro.profissional?.ctpsSerie,
-        [Validators.required],
-      ],
-      fotoProfissional: ['', [Validators.required]],
-      fotoRg: ['', [Validators.required]],
+    jQuery('.datetimepicker').datetimepicker({
+      format: 'DD/MM/YYYY',
+      maxDate: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - 1)
     });
 
     this._loading.emitChange(false);
+  }
+
+  popularForm() {
+    this.profissionalForm.patchValue({
+      nome: this._cadastro.profissional?.nome,
+      sobrenome: this._cadastro.profissional?.sobrenome,
+      dataNascimento: this._cadastro.profissional?.dataNascimento,
+      rg: this._cadastro.profissional?.rg,
+      rgEmissor: this._cadastro.profissional?.rgEmissor,
+      rgDataEmissao: this._cadastro.profissional?.rgDataEmissao,
+      pis: this._cadastro.profissional?.pis,
+      // genero: ['', [Validators.required]],
+      // tipoEmpresa: ['', [Validators.required]],
+      // estadoCivil: ['', [Validators.required]],
+      cnpj: this._cadastro.profissional?.cnpj,
+      ctps: this._cadastro.profissional?.ctps,
+      ctpsSerie: this._cadastro.profissional?.ctpsSerie,
+      // fotoProfissional: ['', [Validators.required]],
+      // fotoRg:
+    });
 
     if (this._cadastro.profissional?.fotoProfissional) {
       this.imagemFotoProfissional = this._cadastro.profissional?.fotoProfissional;
@@ -156,11 +161,6 @@ export class InformacoesGeraisComponent implements OnInit {
     this.generoId = this._cadastro.profissional?.genero.id;
     this.tipoEmpresaId = this._cadastro.profissional?.tipoEmpresa.id;
     this.estadoCivilId = this._cadastro.profissional?.estadoCivil.id;
-
-    jQuery('.datetimepicker').datetimepicker({
-      format: 'DD/MM/YYYY',
-      maxDate: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - 1)
-    });
   }
 
   onLoadFotoProfissional(event: any) {
@@ -222,9 +222,9 @@ export class InformacoesGeraisComponent implements OnInit {
     );
   }
 
-  lpadZero(control: AbstractControl) {
-    let valor = String(Number(control.value)).padStart(9, '0');
-    this.profissionalForm.controls.rg.setValue(valor);
+  lpadZero(control: FormControl) {
+    let valor = control.value.padStart(9, '0');
+    this.profissionalForm.controls.rg.setValue(valor.toUpperCase());
   }
 
   limpar() {
@@ -237,6 +237,12 @@ export class InformacoesGeraisComponent implements OnInit {
     if (this.profissionalForm.controls.rgDataEmissao < this.profissionalForm.controls.dataNascimento) {
       return true;
     }
+  }
+
+  dateChange(control: FormControl, name: string) {
+    jQuery(`#${name}`).on("dp.change", function (event: any) {
+      control.setValue(event?.date?._d.toLocaleDateString());
+    });
   }
 
 }
