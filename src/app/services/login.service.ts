d99cc9d192a11eabb0eb12b23e4ca0f18abfc94e
unknown <eusouoganuto@gmail.com>
2020-08-20 00:17:06 -0300

    Fix de envio duplicado de e-mail; Ajuste para pegar o bearer do localStorage

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,13 +1,14 @@
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
-import { EsqueciMinhaSenha as EsqueciMinhaSenha } from '../classes/esqueci-senha';
-import { NovaSenha } from '../classes/nova-senha';
-import { ResponseApi } from './feat/response-api';
-import { Authorization } from './feat/token';
-import { TokenService } from './token.service';
-import { Login } from '../classes/login.class';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
+import {EsqueciMinhaSenha as EsqueciMinhaSenha} from '../classes/esqueci-senha';
+import {NovaSenha} from '../classes/nova-senha';
+import {ResponseApi} from './feat/response-api';
+import {Authorization} from './feat/token';
+import {TokenService} from './token.service';
+import {Login} from '../classes/login.class';
+import {ValidService} from '../shared/services/shared-valid.service';
 
 @Injectable({
   providedIn: 'root'
@@ -20,28 +21,30 @@ export class LoginService {
 
   private _endpoint: string = `${environment.apiUrl}/login`;
 
-  constructor(private _http: HttpClient, private _tokenService: TokenService) {
-    this._headers = this._headers.set('Token', 'Bearer ' + this._tokenService.getToken());
+  private _authLocalStorage: string = window.localStorage.getItem('auth');
+
+  constructor(private _http: HttpClient, private _tokenService: TokenService, private _validService: ValidService) {
+    this._headers = this._headers.set('Token', 'Bearer ' + this._authLocalStorage);
   }
 
   login(login: Login): Observable<HttpResponse<any>> {
     return this._http.post<HttpResponse<ResponseApi>>(`${this._endpoint}`, login,
-      { headers: this._headers, observe: 'response' });
+      {headers: this._headers, observe: 'response'});
   }
 
   esqueciMinhaSenha(esqueciMinhaSenha: EsqueciMinhaSenha): Observable<HttpResponse<any>> {
     return this._http.post<HttpResponse<ResponseApi>>(`${this._endpoint}/esqueci-minha-senha`, esqueciMinhaSenha,
-      { headers: this._headers, observe: 'response' });
+      {headers: this._headers, observe: 'response'});
   }
 
   validaToken(authorization: Authorization): Observable<HttpResponse<any>> {
     return this._http.post<HttpResponse<ResponseApi>>(`${this._endpoint}/validacao`, authorization,
-      { headers: this._headers, observe: 'response' });
+      {headers: this._headers, observe: 'response'});
   }
 
   novaSenha(novaSenha: NovaSenha): Observable<HttpResponse<any>> {
     return this._http.patch<HttpResponse<ResponseApi>>(`${this._endpoint}/nova-senha`, novaSenha,
-      { headers: this._headers, observe: 'response' });
+      {headers: this._headers, observe: 'response'});
   }
 
 }
