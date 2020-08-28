6f33467326fdc9edc682428afa9133109ab3c7a4
cesar.granelli.dev@gmail.com <cesar.granelli.dev@gmail.com>
2020-08-28 00:05:15 -0300

    implementacao dos detalhes de eventos

---
+++ src/app/admin/eventos/detalhe/evento-detalhe.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/internal/operators/map';
+import { SharedLoadingService } from 'src/app/shared/services/shared-loading.service';
+import { Evento } from '../models/evento.class';
+import { EventoService } from '../services/evento.service';
+
+@Component({
+  selector: 'app-evento-detalhe',
+  templateUrl: './evento-detalhe.component.html',
+  styleUrls: ['./evento-detalhe.component.css']
+})
+export class EventoDetalheComponent implements OnInit {
+
+  public idEvento: number;
+  public escondeTabela: boolean = true;
+  public detalheEvento: Evento;
+
+  constructor(
+    private _loading: SharedLoadingService,
+    private _route: ActivatedRoute,
+    private _eventoService: EventoService
+  ) {
+    this.idEvento = Number(this._route.snapshot.paramMap.get('id'));
+  }
+
+  ngOnInit(): void {
+    this._eventoService.detalhes(this.idEvento).pipe(
+      map(response => {
+        this._loading.emitChange(true);
+        this.detalheEvento = response.body.data;
+      })
+    ).subscribe(
+      null, null, () => {
+      setTimeout(() => {
+        this._loading.emitChange(false);
+        this.escondeTabela = false;
+      });
+    });
+  }
+
+}
