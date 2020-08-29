4fe5031576894ef40cd572df23c02bf0d8d46595
unknown <eusouoganuto@gmail.com>
2020-08-29 18:24:07 -0300

    Ajuste no roteamento dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,40 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CadastroComponent } from './components/cadastro/cadastro.component';
-import { CadastroLoginComponent } from './components/cadastro/login/cadastro-login.component';
-import { CarreiraComponent } from './components/cadastro/profissional/carreira/carreira.component';
-import { ComplementoComponent } from './components/cadastro/profissional/complemento/complemento.component';
-import { EscolaridadeComponent } from './components/cadastro/profissional/escolaridade/escolaridade.component';
-import { ExperienciaComponent } from './components/cadastro/profissional/experiencia/experiencia.component';
-import { InformacoesGeraisComponent } from './components/cadastro/profissional/informacoes-gerais/informacoes-gerais.component';
-import { ConnectaComponent } from './components/connecta/connecta.component';
-import { ContaComponent } from './components/cadastro/profissional/conta/conta.component';
-import { ContatoComponent } from './components/cadastro/profissional/contato/contato.component';
-import { EnderecoComponent } from './components/cadastro/profissional/endereco/endereco.component';
-import { ConfirmacaoCadastroComponent } from './pages/confirmacao-cadastro/confirmacao-cadastro.component';
-import { ConfirmacaoNovaSenhaComponent } from './pages/confirmacao-nova-senha/confirmacao-nova-senha.component';
-import { EsperaConfirmacaoEmailComponent } from './pages/espera-confirmacao-email/espera-confirmacao-email.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CadastroComponent} from './components/cadastro/cadastro.component';
+import {CadastroLoginComponent} from './components/cadastro/login/cadastro-login.component';
+import {CarreiraComponent} from './components/cadastro/profissional/carreira/carreira.component';
+import {ComplementoComponent} from './components/cadastro/profissional/complemento/complemento.component';
+import {EscolaridadeComponent} from './components/cadastro/profissional/escolaridade/escolaridade.component';
+import {ExperienciaComponent} from './components/cadastro/profissional/experiencia/experiencia.component';
+import {InformacoesGeraisComponent} from './components/cadastro/profissional/informacoes-gerais/informacoes-gerais.component';
+import {ConnectaComponent} from './components/connecta/connecta.component';
+import {ContaComponent} from './components/cadastro/profissional/conta/conta.component';
+import {ContatoComponent} from './components/cadastro/profissional/contato/contato.component';
+import {EnderecoComponent} from './components/cadastro/profissional/endereco/endereco.component';
+import {ConfirmacaoCadastroComponent} from './pages/confirmacao-cadastro/confirmacao-cadastro.component';
+import {ConfirmacaoNovaSenhaComponent} from './pages/confirmacao-nova-senha/confirmacao-nova-senha.component';
+import {EsperaConfirmacaoEmailComponent} from './pages/espera-confirmacao-email/espera-confirmacao-email.component';
 
 const routes: Routes = [
-  { path: '', component: ConnectaComponent },
-  { path: 'confirmacao-nova-senha/:token', component: ConfirmacaoNovaSenhaComponent },
-  { path: 'cadastro', component: CadastroComponent },
-  { path: 'cadastro/login', component: CadastroLoginComponent },
-  { path: 'espera-confirmacao-email', component: EsperaConfirmacaoEmailComponent },
-  { path: 'confirmacao-cadastro/:token', component: ConfirmacaoCadastroComponent },
-  { path: 'cadastro/profissionais/:id/informacoes-gerais', component: InformacoesGeraisComponent },
-  { path: 'cadastro/profissionais/:id/endereco', component: EnderecoComponent },
-  { path: 'cadastro/profissionais/:id/contato', component: ContatoComponent },
-  { path: 'cadastro/profissionais/:id/carreira', component: CarreiraComponent },
-  { path: 'cadastro/profissionais/:id/experiencia', component: ExperienciaComponent },
-  { path: 'cadastro/profissionais/:id/escolaridade', component: EscolaridadeComponent },
-  { path: 'cadastro/profissionais/:id/complemento', component: ComplementoComponent },
-  { path: 'cadastro/profissionais/:id/conta', component: ContaComponent }
+  {path: '', component: ConnectaComponent},
+  {path: 'confirmacao-nova-senha/:token', component: ConfirmacaoNovaSenhaComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'cadastro/login', component: CadastroLoginComponent},
+  {path: 'espera-confirmacao-email', component: EsperaConfirmacaoEmailComponent},
+  {path: 'confirmacao-cadastro/:token', component: ConfirmacaoCadastroComponent},
+  {path: 'profissionais/:id/dados-profissionais/informacoes-gerais', component: InformacoesGeraisComponent},
+  {path: 'profissionais/:id/dados-profissionais/endereco', component: EnderecoComponent},
+  {path: 'profissionais/:id/dados-profissionais/contato', component: ContatoComponent},
+  {path: 'profissionais/:id/dados-profissionais/carreira', component: CarreiraComponent},
+  {path: 'profissionais/:id/dados-profissionais/experiencia', component: ExperienciaComponent},
+  {path: 'profissionais/:id/dados-profissionais/escolaridade', component: EscolaridadeComponent},
+  {path: 'profissionais/:id/dados-profissionais/complemento', component: ComplementoComponent},
+  {path: 'profissionais/:id/dados-profissionais/conta', component: ContaComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
