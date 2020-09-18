f1ced421054085842092759ade84f781403458ec
unknown <eusouoganuto@gmail.com>
2020-09-17 21:21:13 -0300

    Info gerais de homecare estruturado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import {EnderecoComponent} from './components/cadastro/profissional/endereco/end
 import {ConfirmacaoCadastroComponent} from './pages/confirmacao-cadastro/confirmacao-cadastro.component';
 import {ConfirmacaoNovaSenhaComponent} from './pages/confirmacao-nova-senha/confirmacao-nova-senha.component';
 import {EsperaConfirmacaoEmailComponent} from './pages/espera-confirmacao-email/espera-confirmacao-email.component';
+import {InformacoesGeraisComponent as HomeCaresInformacoesGeraisComponent} from './homecares/informacoes-gerais/informacoes-gerais.component';
 
 const routes: Routes = [
   {path: '', component: ConnectaComponent},
@@ -29,7 +30,8 @@ const routes: Routes = [
   {path: 'cadastro/profissionais/:id/experiencia', component: ExperienciaComponent},
   {path: 'cadastro/profissionais/:id/escolaridade', component: EscolaridadeComponent},
   {path: 'cadastro/profissionais/:id/complemento', component: ComplementoComponent},
-  {path: 'cadastro/profissionais/:id/conta', component: ContaComponent}
+  {path: 'cadastro/profissionais/:id/conta', component: ContaComponent},
+  {path: 'homecares/informacoes-gerais', component: HomeCaresInformacoesGeraisComponent}
 ];
 
 @NgModule({
--- src/app/homecares/homecares.module.ts
+++ src/app/homecares/homecares.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomecaresComponent } from './homecares.component';
 import { Routes, RouterModule } from '@angular/router';
+import { InformacoesGeraisComponent } from './informacoes-gerais/informacoes-gerais.component';
+import {NgxMaskModule} from 'ngx-mask';
 
 const routes: Routes = [
   { path: 'homecares/:id', component: HomecaresComponent }
 ];
 
 @NgModule({
-  declarations: [HomecaresComponent],
+  declarations: [HomecaresComponent, InformacoesGeraisComponent],
   imports: [
     RouterModule.forChild(routes),
-    CommonModule
+    CommonModule,
+    NgxMaskModule
   ]
 })
 export class HomecaresModule { }
