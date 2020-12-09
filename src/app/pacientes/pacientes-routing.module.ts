41058891271c6ff25e5ce95a11262bd5ad9b6203
Canuto <ganuto@hotmail.com>
2020-12-09 19:40:54 -0300

    Tela de cadastro login paciente criada. necessario refator alguns pontos

--- src/app/pacientes/pacientes-routing.module.ts
+++ src/app/pacientes/pacientes-routing.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PacientesComponent} from './pacientes.component';
+import {CadastroLoginComponent} from './cadastro/login/cadastro-login.component';
 
 const routes: Routes = [
   {path: 'pacientes/:id', component: PacientesComponent},
-  {path: 'pacientes/:id/cadastro/login', component: PacientesComponent}
+  {path: 'pacientes/:id/cadastro/login', component: CadastroLoginComponent}
 ];
 
 @NgModule({
--- src/app/pacientes/pacientes.module.ts
+++ src/app/pacientes/pacientes.module.ts
@@ -2,18 +2,28 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {PacientesComponent} from './pacientes.component';
 import {PacientesRoutingModule} from './pacientes-routing.module';
-import { LoginComponent } from './cadastro/login/login.component';
-import { InformacoesGeraisComponent } from './cadastro/informacoes-gerais/informacoes-gerais.component';
-import { EnderecoComponent } from './cadastro/endereco/endereco.component';
-import { ContatoComponent } from './cadastro/contato/contato.component';
-import { ComplementoComponent } from './cadastro/complemento/complemento.component';
-import { HistoricoMedicoComponent } from './cadastro/historico-medico/historico-medico.component';
+import {CadastroLoginComponent} from './cadastro/login/cadastro-login.component';
+import {InformacoesGeraisComponent} from './cadastro/informacoes-gerais/informacoes-gerais.component';
+import {EnderecoComponent} from './cadastro/endereco/endereco.component';
+import {ContatoComponent} from './cadastro/contato/contato.component';
+import {ComplementoComponent} from './cadastro/complemento/complemento.component';
+import {HistoricoMedicoComponent} from './cadastro/historico-medico/historico-medico.component';
+import {SharedComponentModule} from '../shared/components/shared-component.module';
 
 @NgModule({
-  declarations: [PacientesComponent, LoginComponent, InformacoesGeraisComponent, EnderecoComponent, ContatoComponent, ComplementoComponent, HistoricoMedicoComponent],
+  declarations: [
+    PacientesComponent,
+    CadastroLoginComponent,
+    InformacoesGeraisComponent,
+    EnderecoComponent,
+    ContatoComponent,
+    ComplementoComponent,
+    HistoricoMedicoComponent
+  ],
   imports: [
     CommonModule,
-    PacientesRoutingModule
+    PacientesRoutingModule,
+    SharedComponentModule
   ]
 })
 export class PacientesModule {
