ab617d0f58103b491bbb6984a77655710bf73f3a
Canuto <ganuto@hotmail.com>
2020-12-14 11:41:54 -0300

    Adicionando rota de todos os componentes de pacientes

--- src/app/pacientes/pacientes-routing.module.ts
+++ src/app/pacientes/pacientes-routing.module.ts
@@ -4,12 +4,18 @@ import {PacientesComponent} from './pacientes.component';
 import {CadastroLoginComponent} from './cadastro/login/cadastro-login.component';
 import {InformacoesGeraisComponent} from './cadastro/informacoes-gerais/informacoes-gerais.component';
 import {EnderecoComponent} from './cadastro/endereco/endereco.component';
+import {ContatoComponent} from './cadastro/contato/contato.component';
+import {ComplementoComponent} from './cadastro/complemento/complemento.component';
+import {HistoricoMedicoComponent} from './cadastro/historico-medico/historico-medico.component';
 
 const routes: Routes = [
   {path: 'pacientes/:id', component: PacientesComponent},
   {path: 'pacientes/:id/cadastro/login', component: CadastroLoginComponent},
   {path: 'pacientes/:id/cadastro/informacoes-gerais', component: InformacoesGeraisComponent},
-  {path: 'pacientes/:id/cadastro/endereco', component: EnderecoComponent}
+  {path: 'pacientes/:id/cadastro/endereco', component: EnderecoComponent},
+  {path: 'pacientes/:id/cadastro/contato', component: ContatoComponent},
+  {path: 'pacientes/:id/cadastro/complemento', component: ComplementoComponent},
+  {path: 'pacientes/:id/cadastro/historico-medico', component: HistoricoMedicoComponent}
 ];
 
 @NgModule({
