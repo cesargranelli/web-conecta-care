89b2f89febdd832b3bfd78f618803d83d8b45f54
guilherme.canuto-at-867736945274 <canuto@pop-os.localdomain>
2021-02-12 20:40:37 -0300

    Ajuste no zoom e posicao do mapa

--- src/app/components/connecta/connecta.component.ts
+++ src/app/components/connecta/connecta.component.ts
@@ -8,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 export class ConnectaComponent implements OnInit {
 
   public texto: string = 'Wenceslau Braz - Cuidado com as cargas';
-  public lat: number = -23.8779431;
+  public lat: number = -15.8779431;
   public lng: number = -49.8046873;
   public zoom: number = 15;
   public anoAtual: number;
