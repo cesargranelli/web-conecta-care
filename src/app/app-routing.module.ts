c7950184b1e033586ee3b0c6463a513cd87fa791
Cesar <cesar.granelli.dev@gmail.com>
2021-02-21 01:08:40 -0300

    Adicionado validação do termo e condições de uso
    - Criado checkin para o termo de uso no cadastro do login
    - Alterado o nome da rota de termo-uso para termo-e-condicoes-de-uso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
   {path: '', component: ConnectaComponent},
   {path: 'confirmacao-nova-senha/:token', component: ConfirmacaoNovaSenhaComponent},
   {path: 'espera-confirmacao-email', component: EsperaConfirmacaoEmailComponent},
-  {path: 'termo-uso', component: TermoUsoComponent},
+  {path: 'termo-e-condicoes-de-uso', component: TermoUsoComponent},
   {path: 'confirmacao-cadastro/:token', component: ConfirmacaoCadastroComponent},
   {path: 'cadastro/profissionais/:id/informacoes-gerais', component: InformacoesGeraisComponent},
   {path: 'cadastro/profissionais/:id/endereco', component: EnderecoComponent},
