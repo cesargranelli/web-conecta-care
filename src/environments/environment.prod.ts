1954cb77e01a9c8380c10327c4b742642c7b056b
Cesar <cesar.granelli.dev@gmail.com>
2021-03-01 19:34:58 -0300

    Ajuste no apontamento da api para https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiConnecta: 'https://api-connecta-env-prd.us-east-2.elasticbeanstalk.com',
+  apiConnecta: 'https://api-connecta.connectacare.com.br',
   apiCep: 'https://viacep.com.br',
   siteKey: '6Lfr2gwTAAAAABG4nDVqifJ15HJT7EeXwDTvtch9',
   name: 'PRODUÇÃO'
