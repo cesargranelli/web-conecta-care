f6c5b400b0cc227ce44c31c0d9f571c5cac01a0b
guilherme.canuto-at-867736945274 <canuto@pop-os.localdomain>
2021-05-13 20:54:48 -0300

    Ajustadas rotas de Paciente; Adicionadas funcionalidades de pacientes/dados

--- src/app/pacientes/pacientes.module.ts
+++ src/app/pacientes/pacientes.module.ts
@@ -1,27 +1,28 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgxMaskModule } from 'ngx-mask';
-import { SharedComponentModule } from '../shared/components/shared-component.module';
-import { CadastroComplementoComponent } from './cadastro/complemento/cadastro-complemento.component';
-import { CadastroContatoComponent } from './cadastro/contato/cadastro-contato.component';
-import { CadastroEnderecoComponent } from './cadastro/endereco/cadastro-endereco.component';
-import { CadastroHistoricoMedicoComponent } from './cadastro/historico-medico/cadastro-historico-medico.component';
-import { CadastroInformacoesGeraisComponent } from './cadastro/informacoes-gerais/cadastro-informacoes-gerais.component';
-import { PacientesRoutingModule } from './pacientes-routing.module';
-import { PacientesComponent } from './pacientes.component';
-import { FormContatoComponent } from './shared/components/forms/contato/form-contato.component';
-import { DadosComponent } from './dados/dados.component';
-import { ComplementoComponent } from './dados/complemento/complemento.component';
-import { ContatoComponent } from './dados/contato/contato.component';
-import { EnderecoComponent } from './dados/endereco/endereco.component';
-import { HistoricoMedicoComponent } from './dados/historico-medico/historico-medico.component';
-import { InformacoesGeraisComponent } from './dados/informacoes-gerais/informacoes-gerais.component';
-import { LoginComponent } from './dados/login/login.component';
-import { FormInformacoesGeraisComponent } from './shared/components/forms/informacoes-gerais/form-informacoes-gerais.component';
-import { FormHistoricoMedicoComponent } from './shared/components/forms/historico-medico/form-historico-medico.component';
-import { FormComplementoComponent } from './shared/components/forms/complemento/form-complemento.component';
-import { FormEnderecoComponent } from './shared/components/forms/endereco/form-endereco.component';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgxMaskModule} from 'ngx-mask';
+import {SharedComponentModule} from '../shared/components/shared-component.module';
+import {CadastroComplementoComponent} from './cadastro/complemento/cadastro-complemento.component';
+import {CadastroContatoComponent} from './cadastro/contato/cadastro-contato.component';
+import {CadastroEnderecoComponent} from './cadastro/endereco/cadastro-endereco.component';
+import {CadastroHistoricoMedicoComponent} from './cadastro/historico-medico/cadastro-historico-medico.component';
+import {CadastroInformacoesGeraisComponent} from './cadastro/informacoes-gerais/cadastro-informacoes-gerais.component';
+import {PacientesRoutingModule} from './pacientes-routing.module';
+import {PacientesComponent} from './pacientes.component';
+import {FormContatoComponent} from './shared/components/forms/contato/form-contato.component';
+import {DadosComponent} from './dados/dados.component';
+import {ComplementoComponent} from './dados/complemento/complemento.component';
+import {ContatoComponent} from './dados/contato/contato.component';
+import {EnderecoComponent} from './dados/endereco/endereco.component';
+import {HistoricoMedicoComponent} from './dados/historico-medico/historico-medico.component';
+import {InformacoesGeraisComponent} from './dados/informacoes-gerais/informacoes-gerais.component';
+import {LoginComponent} from './dados/login/login.component';
+import {FormInformacoesGeraisComponent} from './shared/components/forms/informacoes-gerais/form-informacoes-gerais.component';
+import {FormHistoricoMedicoComponent} from './shared/components/forms/historico-medico/form-historico-medico.component';
+import {FormComplementoComponent} from './shared/components/forms/complemento/form-complemento.component';
+import {FormEnderecoComponent} from './shared/components/forms/endereco/form-endereco.component';
+import {ProfissionaisModule} from "../profissionais/profissionais.module";
 
 @NgModule({
   declarations: [
@@ -50,7 +51,8 @@ import { FormEnderecoComponent } from './shared/components/forms/endereco/form-e
     SharedComponentModule,
     FormsModule,
     ReactiveFormsModule,
-    NgxMaskModule
+    NgxMaskModule,
+    ProfissionaisModule
   ]
 })
 export class PacientesModule {
