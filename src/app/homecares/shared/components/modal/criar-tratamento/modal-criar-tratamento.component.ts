36044547ee3826114820b3da62f3e824b7d0c620
Cesar <cesar.granelli.dev@gmail.com>
2021-09-18 17:04:07 -0300

    Ajuste na busca de paciente pelo CPF no modal de novo tratamento

--- src/app/homecares/shared/components/modal/criar-tratamento/modal-criar-tratamento.component.ts
+++ src/app/homecares/shared/components/modal/criar-tratamento/modal-criar-tratamento.component.ts
@@ -1,13 +1,14 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TratamentoAberto } from 'src/app/homecares/classes/tratamento-aberto.class';
 import { PacienteService } from 'src/app/homecares/services/paciente.service';
+import { TratamentoService } from 'src/app/homecares/services/tratamento.service';
 import { Paciente } from 'src/app/pacientes/classes/paciente.class';
 import { SharedLoadingService } from 'src/app/shared/services/shared-loading.service';
-import Swal from 'sweetalert2';
-import { TratamentoService } from 'src/app/homecares/services/tratamento.service';
 import { SharedValidService } from 'src/app/shared/services/shared-valid.service';
-import { TratamentoAberto } from 'src/app/homecares/classes/tratamento-aberto.class';
+import Swal from 'sweetalert2';
 
 declare var jQuery: any;
 
@@ -70,13 +71,25 @@ export class ModalCriarTratamentoComponent implements OnInit {
         }, null);
         break;
     }
-    this.loading.emitChange(false);
   }
 
   private consultaTratamentoEmAberto(paciente: Paciente) {
     this.tratamentoService.consultarTratamentoEmAberto(String(paciente?.id), String(this.sharedValidService.getValid()?.id))
       .subscribe(tratamentoEmAberto => {
         this.redirecionamento(tratamentoEmAberto?.body?.data);
+      }, (httpErrorResponse: HttpErrorResponse) => {
+        if (httpErrorResponse.error.status == 412) {
+          this.redirecionamento(null);
+        } else {
+          Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Ops! Ocorreu um erro inexperrado',
+            showConfirmButton: true,
+          });
+          console.log(httpErrorResponse);
+          this.loading.emitChange(false);
+        }
       });
   }
 
